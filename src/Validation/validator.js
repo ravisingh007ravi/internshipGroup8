4697f42eba82ee571ab7f4193c5c70a376d9d553
ravisingh007ravi <ravisingh007ravi.com>
2022-11-22 10:59:23 +0530

    midified in validator

--- src/Validation/validator.js
+++ src/Validation/validator.js
@@ -1,57 +1,42 @@
-//__________________________ Import  ___________________________________________
-
+//<---------------------------------Import------------------------------>//
 const mongoose=require('mongoose');
 
-//__________________________ Validations : Name  ___________________________________________
-
+//<--------------------------Validations : Name-------------------------->//
 const isValidName=function(name){
     const regexName=/^[a-zA-Z]+$/;;
     return regexName.test(name)
 }
 
-  //__________________________ Validations : Mobile No ___________________________________________
-
+//<--------------------------Validations : Mobile No-------------------------->//
 const isValidMobileNo=function(mobile){
     const regexMob=/^[0-9]{10}+$/;
     return regexMob.test(mobile);
 }
 
-//__________________________ Validations : Email  ___________________________________________
-
+//<---------------------------Validations : Email----------------------------->//
 const isValidEmail=function(email){
     const regexEmail=/[a-zA-Z_1-90]{3,}@[A-za-z]{3,}[.]{1}[a-zA-Z]{2,}/
     return regexEmail.test(email)
 }
 
-//__________________________ Validations : Password  ___________________________________________
-
+//<---------------------------Validations : Password----------------------------->//
 const isValidPassword = function (password) {
     const passwordRegex =
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     return passwordRegex.test(password);
   };
   
-  //__________________________ Validations : Values ___________________________________________
-  
-  const isValid = function (value) {
+//<---------------------------Validations : Values------------------------------->//
+const isValid = function (value) {
     if (typeof value === "undefined" || value === null) return false;
     if (typeof value == "string" && value.trim().length === 0) return false;
     return true;
   };
   
-  //__________________________ Validations :  ObjectId ___________________________________________
-  
-  const isValidObjectId = function (objectId) {
+//<---------------------------Validations :  ObjectId------------------------------->//
+const isValidObjectId = function (objectId) {
     return mongoose.Types.ObjectId.isValid(objectId);
   };
   
-  //__________________________ Export : Modules  ___________________________________________
-  
-  module.exports = {
-    isValid,
-    isValidMobileNo,
-    isValidEmail,
-    isValidName,
-    isValidPassword,
-    isValidObjectId,
-  };
+//<------------------------------Export : Modules---------------------------------->//
+module.exports = {isValid,isValidMobileNo,isValidEmail,isValidName,isValidPassword,isValidObjectId};
